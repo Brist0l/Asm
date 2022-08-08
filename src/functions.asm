85073330f1397c233343d7126b80b60387b564f8
Brist0l <gautamthegreat21@gmail.com>
2022-08-08 14:36:29 +0530

    added more stuff

--- src/functions.asm
+++ src/functions.asm
@@ -9,6 +9,32 @@ atoi:
 	mov 	esi,eax
 	mov 	eax,0
 	mov 	ecx,0
+.multiplyLoop: 		;local label
+	xor 	ebx,ebx ;resets both lower and upper bytes to 0 of ebx
+	mov 	bl,[esi+ecx]
+	cmp 	bl,48  ; checks if its ascii 0
+	jl 	.finished
+	cmp 	bl,57 ; checks if its ascii 9
+	jg 	.finished
+	sub     bl, 48          ; convert ebx register's lower half to decimal representation of ascii value
+    	add     eax, ebx        ; add ebx to our interger value in eax
+    	mov     ebx, 10         ; move decimal value 10 into ebx
+    	mul     ebx             ; multiply eax by ebx to get place value
+   	inc     ecx             ; increment ecx (our counter register)
+    	jmp     .multiplyLoop   ; continue multiply loop
+ 
+.finished:
+    	cmp     ecx, 0          ; compare ecx register's value against decimal 0 (our counter register)
+    	je      .restore        ; jump if equal to 0 (no integer arguments were passed to atoi)
+    	mov     ebx, 10         ; move decimal value 10 into ebx
+    	div     ebx             ; divide eax by value in ebx (in this case 10)
+ 
+.restore:
+    	pop     esi             ; restore esi from the value we pushed onto the stack at the start
+    	pop     edx             ; restore edx from the value we pushed onto the stack at the start
+    	pop     ecx             ; restore ecx from the value we pushed onto the stack at the start
+    	pop     ebx             ; restore ebx from the value we pushed onto the stack at the start
+    	ret
 
 ;---------------------
 ; void iprint
